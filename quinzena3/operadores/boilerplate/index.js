369a856774222ceeb4579059f02a405ef699a69d
Livia Zeviani <liviapavinizeviani@gmail.com>
2020-09-17 18:55:33 -0300

    exercicios operadores incompleto

---
+++ quinzena3/operadores/boilerplate/index.js
@@ -0,0 +1,40 @@
+console.log("Oie, eu só sirvo pra saber se as coisas funcionaram!")
+
+/* 1) false
+      false
+      true
+      boolean
+*/
+
+/* 2) não está definido
+      null
+      11
+      3
+      3,19,5,6,7,8,9,10,11,12
+      6
+*/
+
+/* 1 Escrita de código
+const user = Number(prompt(" Qual é a sua idade?" ));
+const amigo = Number(prompt(" Quantos anos tem seu melhor amigo ou amiga?" ));
+
+const idadesIguais = user == amigo;
+const idadeMaior = user > amigo;
+const idadeMenor = user < amigo;
+
+user == amigo == false;
+user > amigo == true;
+user < amigo == false;
+
+let resultado = user > amigo;
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", resultado );
+*/
+
+const par = Number(prompt("Digite um número que seja par: "));
+
+let resultado = par%2 == 0;
+let naoresultado = par%2 != 0;
+
+console.log("Ok, esse número é par", resultado);
+console.log("Vish, isso não é um número par", naoresultado);
