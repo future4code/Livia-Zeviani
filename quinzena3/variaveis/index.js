69597be877f4c21c1b427b2880fcfc841e53771c
Livia Zeviani <liviapavinizeviani@gmail.com>
2020-09-16 22:48:45 -0300

    exercicios variáveis

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,62 @@
+//exercicio 1//
+
+/*10
+10, 5
+*/
+
+//10,10,10//
+
+/*var nome = ''
+var idade = ''
+
+console.log(typeof(nome))
+console.log(typeof(idade))
+*/
+
+
+/* d) foi impresso que as variáveis
+      são strings
+*/
+
+/*const nomeA = prompt("digite seu nome:")
+const idadeB = prompt("digite sua idade:")
+
+console.log(typeof(nomeA)) 
+console.log(typeof(idadeB))
+/*
+/*strings representam textos
+*/
+
+
+
+
+   /* var a = prompt("Digite seu nome:","Nome");
+    var b = prompt("QUantos anos vocÊ tem:");
+    document.write("Olá, " + a + " " + "vocÊ tem " + b +  " anos" +  "<br />");
+    */
+
+
+/*
+    var c = prompt("Seu endereço:","Endereço");
+    var d = prompt("Sua cor favorita:");
+    var e = prompt("Sua comida favorita:");
+    var f = prompt("Você namora?");
+    var g = prompt("Nome do seu pet:");
+    document.write("Qual " + " o" + " seu" + " endereço" + "?" + " Resposta: " + c +  "<br />" + "Qual " + "sua " + "cor " + "favorita " + "? " + " Resposta: " + d +  "<br />" + "Qual " + "sua " + "cor " + "favorita " + "? " + " Resposta: " + e +  "<br />" + "Você " + "namora " + "? " + " Resposta: " + f +  "<br />" + "Qual " + "nome " + "do " + "seu " + "pet " + "? " + " Resposta: " + g +  "<br />"  );
+*/
+//exercicio 3//
+/*let array = ["arroz", "macarrão", "ovo", "okonomyiaki", "feijão"]
+console.log(array[0], array[1], array[2], array[3], array[4])*/
+
+/*
+let array = ["arroz e feijão", "okonomiyaki", "lamen", "gohan", "poke"]
+document.write("Essas, " + "são " + "minhas " +  " comidas" + " favoritas:" + array[0] + "<br />" + array[1] + "<br />" + array[2] + "<br />" + array[3] + "<br />" + array[4] + "<br />" );
+*/
+
+/*var f = prompt("Qual sua comida favorita?");
+let array = ["arroz e feijão", "okonomiyaki", "lamen", "gohan", "poke"]
+
+array[1] = f
+console.log(array[0], array[1], array[2], array[3], array[4])
+
+*/
