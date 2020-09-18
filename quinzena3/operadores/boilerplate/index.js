6fcf6b7ea6b1cca159127dcadced6b2c322371d9
Livia Zeviani <liviapavinizeviani@gmail.com>
2020-09-18 01:22:55 -0300

    finalizei os exercícios obrigatórios

--- quinzena3/operadores/boilerplate/index.js
+++ quinzena3/operadores/boilerplate/index.js
@@ -31,7 +31,7 @@ let resultado = user > amigo;
 console.log("Sua idade é maior do que a do seu melhor amigo?", resultado );
 */
 
-/* 2 a) 
+/* 2  
 const par = Number(prompt("Digite um número que seja par: "));
 
 let resultado = par%2 == 0;
@@ -40,3 +40,27 @@ let naoresultado = par%2 != 0;
 console.log("Ok, esse número é par", resultado);
 console.log("Vish, isso não é um número par", naoresultado);
 */
+
+/* 3
+let listaDeTarefas = [];
+
+listaDeTarefas.push(prompt("Digite uma tarefa de sua lista:"));
+listaDeTarefas.push(prompt("Digite outra tarefa de sua lista:"));
+listaDeTarefas.push(prompt("Digite mais uma tarefa de sua lista:"));
+
+console.log(listaDeTarefas);
+
+
+listaDeTarefas.splice(Number(prompt("índice:")), 1)
+
+console.log(listaDeTarefas);
+*/
+
+const emailDoUsuario = String(prompt("Digite seu e-mail: "));
+const nomeDoUsuario = String(prompt("Digite seu nome: "));
+
+console.log("O e-mail " + emailDoUsuario + " " + "foi cadastrado com sucesso." + " " + "Seja bem-vinda(o), " + nomeDoUsuario);
+
+
+
+
