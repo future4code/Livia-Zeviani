fee3f51da4a2e00d7a3ff4cd3b3bc25b4205c93e
Livia Zeviani <liviapavinizeviani@gmail.com>
2020-10-11 02:40:20 -0300

    criada a pasta, os arquivos e o primeiro exercicio de interpretação

---
+++ quinzena4/lista-de-exercicios/lista.js
@@ -0,0 +1,7 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+/*
+1. Converte valor de dólares para reais. 
+Aparece uma mensagem para o usuário "Informe o valor da cotação do dólar"
+então o valor já é convertido em reais e retorna como R$valor.
+Nesse caso específico vai mostrar a conversão de 100 dólares em reais.
+*/
