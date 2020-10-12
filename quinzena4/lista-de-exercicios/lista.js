3465484c977f7c95ec75a0b57c743852412b8c6e
Livia Zeviani <liviapavinizeviani@gmail.com>
2020-10-12 00:54:16 -0300

    Exercícios de interpretação de código resolvidos

--- quinzena4/lista-de-exercicios/lista.js
+++ quinzena4/lista-de-exercicios/lista.js
@@ -4,4 +4,25 @@
 Aparece uma mensagem para o usuário "Informe o valor da cotação do dólar"
 então o valor já é convertido em reais e retorna como R$valor.
 Nesse caso específico vai mostrar a conversão de 100 dólares em reais.
+
+2. O código apresenta o switch, que é uma condição. A partir do tipo 
+de investimento que o usuário escolhe ele vai retornar o valor referente ao 
+investimento. Nesse caso específico há duas constantes, a que retornará o novoMontante e 
+o que retornará o segundoMontante. O novoMontante retornará 165 (pois é 150 x 1.1)
+enquanto o segundoMontante retornará TIPO DE INVESTIMENTO INFORMADO INCORRETO 
+
+3. O código tem um array chamado numeros com alguns números e dois arrays vazios
+(array1 e array2).
+
+O código então faz um loop com for em que pede que no array1 sejam colocados todos
+os números pares (a partir daquele array numeros já declarado anteriormente) e
+quando não forem pares irem para o array2.
+O que será impresso no console será a quantidade de números dentro dos 3 arrays
+(no array numeros, no array1 e no array2)
+
+4. Nesse caso o código apresenta um array e pede para que ocorra um loop com a
+condição de que o numero seja menor que positivo infinito e então ele seja o numero1
+e outro loop com numero maior que 0 e então ele será o número2.
+O que o console irá imprimir é -10 e 159.
+
 */
