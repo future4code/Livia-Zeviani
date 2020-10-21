d5757fa9b90d9f93defffc49e2113cca0b6febb0
Lívia <liviapavinizeviani@gmail.com>
2020-10-20 21:24:55 -0300

    Exercício Insta4 incompleto

---
+++ insta4 2/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './App.css';
+import Post from './components/Post/Post';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={'app-container'}>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+<Post
+          nomeUsuario={'Ada Lovelace'}
+          fotoUsuario={'https://picsum.photos/50/50?a=2'}
+          fotoPost={'https://picsum.photos/200/150?a=2'}
+        />
+
+        <Post
+          nomeUsuario={'Grace Hopper'}
+          fotoUsuario={'https://picsum.photos/50/50?a=3'}
+          fotoPost={'https://picsum.photos/200/150?a=3'}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
